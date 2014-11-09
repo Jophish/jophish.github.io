4e49a186f38e89e88673dbab7732f7ddea203d37
joe <jophish126@gmail.com>
2014-11-09 17:57:19 -0500

    thanks @peterldowns for the spooky shit

--- demo.js
+++ demo.js
@@ -7,6 +7,7 @@ var numCircles = 200,
 var offset = 500, colorNum = 0 ;
 var back = two.makeGroup();
 var circs = two.makeGroup();
+var FRAMECOUNT = 0;
 
 function rectMove(s){
 	return (Math.cos(s * Math.PI / 180)*100)
@@ -37,8 +38,35 @@ circs.add(circle);
 //circs.center(500);
 
 
+mousePosition = {x: -500, y: -500};
+
+window.addEventListener("mousemove", function(event) {
+	mousePosition.x = event.clientX;
+	console.log('hello');
+	mousePosition.y = event.clientY;
+	//console.log(mousePosition);
+}, false);
+
+function makeCirc(event) {
+	var x = mousePosition.x;
+	var y = mousePosition.y;
+	if (event) {
+		x = event.clientX;
+		y = event.clientY;
+	}
+	circle = two.makeCircle(x, y, (Math.cos(FRAMECOUNT / 60) + 1) * 100);
+	circle.fill = getRandomColor();
+	circle.opacity = 0.4;
+	//circleVect[numCircles] = circle;
+	//numCircles += 1;
+	//circs.add(circle);
+}
 
 two.bind("update", function (frameCount){
+	FRAMECOUNT = frameCount;
+	makeCirc();
+
+
 	var pos = rectMove((place++)*5);
 	
 	rect2.translation.x = pos + offset + 300;
@@ -46,18 +74,18 @@ two.bind("update", function (frameCount){
     rect2.rotation = rectMove(place)/10;
 	circs.scale +=  Math.cos((scaleOffsets[x] + increment++/100.0)*(Math.PI / 180)*10);
 	for (x = 0; x < numCircles; x++){
-	circleVect[x].scale = Math.cos((scaleOffsets[x] + increment++/100.0)*(Math.PI / 180)*10);
-	circleVect[x].fill = getRandomColor();
-	circleVect[x].translation.x += Math.cos(increment*10)*30
-	circleVect[x].translation.y += Math.sin(increment*10)*30
+		circleVect[x].scale = Math.cos((scaleOffsets[x] + increment++/100.0)*(Math.PI / 180)*10);
+		circleVect[x].fill = getRandomColor();
+		circleVect[x].translation.x += Math.cos(increment*10)*30
+		circleVect[x].translation.y += Math.sin(increment*10)*30
 	}
 	rect2.scale = 10 + Math.cos((increment/50.0)*(Math.PI / 180)*80);
 	colorNum++;
 	if (colorNum == 20){
 		rect2.fill = getRandomColor();
 		colorNum = 0;
-		}
+	}
 	
-	});
+});
 
-	two.play()
+two.play()
